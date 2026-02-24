49e086b8882f48cd7b8c4b60c2ed00ab5fdafd44
andreasalvi99 <andrea.salvi.r@gmail.com>
2026-02-24 12:18:16 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <section>
+        <div className="container text-center">
+          <h1>CIAOOO</h1>
+        </div>
+      </section>
+    </>
+  );
+}
