24d16f7b2499eaa9b4cbe761749da6b3b6d53e9a
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-24 13:09:26 +0100

    set state for search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+
 export default function App() {
+  const [searchInput, setSearchInput] = useState();
+
   return (
     <>
       <section>
         <div className="container text-center">
           <h1>CIAOOO</h1>
+          <div className="input-group mb-3">
+            <input
+              value={searchInput}
+              onChange={(e) => {
+                setSearchInput(e.target.value);
+              }}
+              // ^
+              type="text"
+              className="form-control"
+              placeholder="Recipient’s username"
+              aria-label="Recipient’s username"
+              aria-describedby="button-addon2"
+            />
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              id="button-addon2"
+            >
+              Button
+            </button>
+          </div>
         </div>
       </section>
     </>
