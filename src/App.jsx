7aa74c16229c11a71b678289728638a84bd92022
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-24 13:19:42 +0100

    add form and prevent default

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
 import { useState } from "react";
 
 export default function App() {
-  const [searchInput, setSearchInput] = useState();
+  const [searchInput, setSearchInput] = useState(); //^ Controllo valore di ricerca per nome
+  const [searchedFilms, setSearchedFilms] = useState([]); //^ Stato dell'array dei film cercati, all'inizio è vuoto
 
   function handleSerachInput(e) {
-    return setSearchInput(e.target.value);
+    return setSearchInput(e.target.value); //^ Funzione per settare lo stato sul valore inserito dall'utente
   }
 
   return (
     <>
       <section>
-        <div className="container text-center">
-          <h1>CIAOOO</h1>
-          <div className="input-group mb-3">
-            <input
-              value={searchInput}
-              onChange={handleSerachInput}
-              // ^
-              type="text"
-              className="form-control"
-              placeholder="Recipient’s username"
-              aria-label="Recipient’s username"
-              aria-describedby="button-addon2"
-            />
-            <button
-              className="btn btn-outline-secondary"
-              type="button"
-              id="button-addon2"
-            >
-              Button
-            </button>
+        <form
+          className="form-control"
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("ciao");
+          }}
+        >
+          <div className="container text-center">
+            <h1>CIAOOO</h1>
+            <div className="input-group mb-3">
+              <input
+                value={searchInput}
+                onChange={handleSerachInput}
+                //^
+                type="text"
+                className="form-control"
+                placeholder="Recipient’s username"
+                aria-label="Recipient’s username"
+                aria-describedby="button-addon2"
+              />
+              <button
+                //^
+                className="btn btn-outline-secondary"
+                type="button"
+                id="button-addon2"
+              >
+                Button
+              </button>
+            </div>
           </div>
-        </div>
+        </form>
       </section>
     </>
   );
