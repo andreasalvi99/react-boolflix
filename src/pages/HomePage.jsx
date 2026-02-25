9ffc35f0df4a1e58e941a28cdb7da2919028e1f6
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-25 10:55:00 +0100

    create empty state

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,44 +9,65 @@ export default function HomePage() {
   return (
     <>
       <Header />
-      <section>
-        <div className="container text-center my-5">
-          <h2 className="my-5 text-start">
-            <span>Films</span>
-          </h2>
-          <div className="row row-cols-3 g-3">
-            {searchedFilms.map((searchedFilm) => {
-              return (
-                <FilmCard
-                  img={searchedFilm.poster_path}
-                  key={searchedFilm.id}
-                  title={searchedFilm.title}
-                  original_title={searchedFilm.original_title}
-                  rating={searchedFilm.vote_average}
-                  overview={searchedFilm.overview}
-                />
-              );
-            })}
+      {searchedFilms.length > 0 || searchedDramas.length > 0 ? (
+        <>
+          <section>
+            <div className="container text-center my-5">
+              {searchedFilms && (
+                <>
+                  <h2 className="my-5 text-start">
+                    <span>Films</span>
+                  </h2>
+                  <div className="row row-cols-3 g-3">
+                    {searchedFilms.map((searchedFilm) => {
+                      return (
+                        <FilmCard
+                          img={searchedFilm.poster_path}
+                          key={searchedFilm.id}
+                          title={searchedFilm.title}
+                          original_title={searchedFilm.original_title}
+                          rating={searchedFilm.vote_average}
+                          overview={searchedFilm.overview}
+                        />
+                      );
+                    })}
+                  </div>
+                </>
+              )}
+
+              {searchedDramas && (
+                <>
+                  <h2 className="my-5 text-start">
+                    <span>TV Series</span>
+                  </h2>
+                  <div className="row row-cols-3 g-3 my-5">
+                    {searchedDramas.map((searchedDrama) => {
+                      return (
+                        <DramaCard
+                          img={searchedDrama.poster_path}
+                          key={searchedDrama.id}
+                          name={searchedDrama.name}
+                          original_name={searchedDrama.original_name}
+                          rating={searchedDrama.vote_average}
+                          overview={searchedDrama.overview}
+                        />
+                      );
+                    })}
+                  </div>
+                </>
+              )}
+            </div>
+          </section>
+        </>
+      ) : (
+        <section id="empty-state">
+          <div className="container d-flex justify-content-center align-items-center">
+            <span className="text-white fs-1">
+              Centinaia di migliaia di Film e Serie TV a tua disposizione...
+            </span>
           </div>
-          <h2 className="my-5 text-start">
-            <span>TV Series</span>
-          </h2>
-          <div className="row row-cols-3 g-3 my-5">
-            {searchedDramas.map((searchedDrama) => {
-              return (
-                <DramaCard
-                  img={searchedDrama.poster_path}
-                  key={searchedDrama.id}
-                  name={searchedDrama.name}
-                  original_name={searchedDrama.original_name}
-                  rating={searchedDrama.vote_average}
-                  overview={searchedDrama.overview}
-                />
-              );
-            })}
-          </div>
-        </div>
-      </section>
+        </section>
+      )}
     </>
   );
 }
